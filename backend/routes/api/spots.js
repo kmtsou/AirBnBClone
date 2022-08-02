3026a11ffcaa71d0f444104d8e0429d1dc9b6fb7
kevin <kmtsou@sbcglobal.net>
2022-08-02 09:47:19 -0700

    figured out sequelize literal for get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-const { User, Review, Booking, Spot, Image } = require('../../db/models');
+const { User, Review, Booking, Spot, Image, sequelize } = require('../../db/models');
 
 router.get('/', async(req, res) => {
-    const allSpots = await Spot.findAll();
+    const allSpots = await Spot.findAll({
+        attributes: {
+            include: [
+                [
+                    // sequelize.fn("AVG", sequelize.col("Reviews.stars")), "avgRating"
+                    sequelize.literal(`(
+                        SELECT AVG(stars)
+                        FROM reviews
+                        WHERE reviews.spotId = spot.id
+                    )`),
+                    "avgRating"
+                ]
+            ]
+        },
+        include: [
+            {
+                model: Review,
+                attributes: []
+            },
+            {
+                model: Image,
+                attributes: ['url']
+            }
+        ]
+    });
+
+    res.json(allSpots);
 })
 
 module.exports = router;
