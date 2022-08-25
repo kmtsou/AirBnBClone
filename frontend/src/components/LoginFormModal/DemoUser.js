1f700ec8d2fa7a37741f509e7b54ee137b2fb923
kevin <kmtsou@sbcglobal.net>
2022-08-25 10:46:41 -0700

    added redirects on login

--- frontend/src/components/LoginFormModal/DemoUser.js
+++ frontend/src/components/LoginFormModal/DemoUser.js
@@ -1,22 +1,27 @@
 import { useDispatch } from "react-redux";
 import { login } from "../../store/session";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const DemoUser = () => {
     const dispatch = useDispatch();
+    const history = useHistory();
     const [errors, setErrors] = useState([]);
 
-    const loginDemoUser = (e) => {
+    const loginDemoUser = async (e) => {
         e.preventDefault();
         const demoCredentials = {
             credential: 'Demo-lition',
             password: 'password'
         }
-        dispatch(login(demoCredentials)).catch(
+        let loggedInDemo = await dispatch(login(demoCredentials)).catch(
             async (res) => {
                 const data = await res.json();
                 if (data && data.errors) setErrors(data.errors);
             })
+        if (loggedInDemo) {
+            history.push('/');
+        }
     };
 
     return (
