a22a8e66d2e80654cc66879776979ba9482e92b5
kevin <kmtsou@sbcglobal.net>
2023-02-10 14:54:00 -0800

    changing model from string to text

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -54,7 +54,7 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false
     },
     price: {
